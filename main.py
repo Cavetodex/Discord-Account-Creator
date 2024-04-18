bebc24d0347d16603a35941686c3de8e0adacb37
Cavetodex <167407997+Cavetodex@users.noreply.github.com>
2024-04-18 09:24:10 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import subprocess
+import os
+
+def execute_script(script_path):
+    try:
+        subprocess.run(['python', script_path], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Fehler beim Ausführen von {script_path}: {e}")
+
+def main():
+    script_directory = os.path.join(os.path.dirname(__file__), 'source')
+    script_name = 'stub.py'
+    script_path = os.path.join(script_directory, script_name)
+    execute_script(script_path)
+
+if __name__ == "__main__":
+    main()
