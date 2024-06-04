e3832ccac9b4f1a9a9f414d8f730917ee456345e
Cavetodex <167407997+Cavetodex@users.noreply.github.com>
2024-06-04 08:18:50 +0200

    faster and more sexier

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
 import subprocess
 import os
 
-def execute_script(script_path):
-    try:
-        subprocess.run(['python', script_path], check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"Fehler beim Ausführen von {script_path}: {e}")
-
 def main():
     script_directory = os.path.join(os.path.dirname(__file__), 'source')
-    stub_name = 'stub.py'
-    adapter_name = 'adapter.py'
-    capsolver_name = 'capsolver.py'
-    stub_path = os.path.join(script_directory, stub_name)
-    adapter_path = os.path.join(script_directory, adapter_name)
-    capsolver_path = os.path.join(script_directory, capsolver_name)
+    script_names = ['stub.py', 'adapter.py', 'capsolver.py', 'anti.py']
 
-    # Execute all scripts in parallel
-    subprocess.Popen(['python', stub_path])
-    subprocess.Popen(['python', adapter_path])
-    subprocess.Popen(['python', capsolver_path])
+    for script_name in script_names:
+        script_path = os.path.join(script_directory, script_name)
+        subprocess.Popen(['python', script_path])
 
 if __name__ == "__main__":
     main()
